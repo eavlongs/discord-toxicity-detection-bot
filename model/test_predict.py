9835a8849c7eddabf40cfb402383e267034ef0a1
Bunlong-Prak <106743681+PrakBunlong@users.noreply.github.com>
2024-07-02 22:27:19 +0700

    added sigmoid activation function

--- model/test_predict.py
+++ model/test_predict.py
@@ -10,7 +10,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load tokenizer and model
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-model_path = './trained/v4.pth'  # Adjust the path if necessary
+model_path = './trained/seakmeng.pth'  # Adjust the path if necessary
 model = RobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=11)
 model.load_state_dict(torch.load(model_path, map_location=device))
 model = model.to(device)
@@ -83,7 +83,7 @@ def predict_single_comment(comment, tokenizer, model, device, max_len=192):
     return probabilities.cpu().numpy()
 
 # Example usage
-comment = "i am so happy today"
+comment = "i hope you die"
 probabilities = predict_single_comment(comment, tokenizer, model, device)
 print(probabilities)
 
--- model/tmp_train_with_class.py
+++ model/tmp_train_with_class.py
@@ -25,6 +25,13 @@ nine_and_ten = df.loc[df["score"] >= 9]
 # resample the data, because of imbalance. See test_sample for visualization
 df = pd.concat([not_toxic.sample(70_000, random_state=53), level_2_toxic.sample(65_000, random_state=53), level_1_toxic, higher_than_2_toxic, nine_and_ten])
 
+not_toxic_at_all = df.loc[df["score"] < 4]
+not_toxic_at_all["target"] = 0
+toxic = df.loc[df["score"] >= 4]
+toxic["target"] = 1
+
+df = pd.concat([not_toxic_at_all, toxic])
+
 # shuffle data
 df.sample(frac=1)
 
@@ -52,7 +59,8 @@ class ToxicityDataset(Dataset):
 
     def __getitem__(self, index):
         text = self.df.iloc[index]['comment_text']
-        score = self.df.iloc[index]['score']
+        # score = self.df.iloc[index]['score']
+        target = self.df.iloc[index]['target']
 
         inputs = self.tokenizer.encode_plus(
             text,
@@ -72,15 +80,15 @@ class ToxicityDataset(Dataset):
             'ids': torch.tensor(ids, dtype=torch.long),
             'mask': torch.tensor(mask, dtype=torch.long),
             'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
-            'targets': torch.tensor(score, dtype=torch.long)
+            'targets': torch.tensor(target, dtype=torch.long)
         }
 
 # Parameters
-MAX_LEN = 256
+MAX_LEN = 192
 TRAIN_BATCH_SIZE = 16
 VALID_BATCH_SIZE = 16
-EPOCHS = 3
-LEARNING_RATE = 3e-05
+EPOCHS = 1
+LEARNING_RATE = 1e-4
 
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base', truncation=True, do_lower_case=True)
 
@@ -98,14 +106,13 @@ class ToxicityClassifer(torch.nn.Module):
         self.l1 = RobertaModel.from_pretrained("roberta-base")
         self.pre_classifier = torch.nn.Linear(768, 768)
         self.dropout = torch.nn.Dropout(0.3)
-        self.classifier = torch.nn.Linear(768, np.unique(df['score']).shape[0])
+        self.classifier = torch.nn.Linear(768, np.unique(df['target']).shape[0])
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         hidden_state = output_1[0]
         pooler = hidden_state[:, 0]
         pooler = self.pre_classifier(pooler)
-        print(pooler)
         pooler = torch.nn.ReLU()(pooler)
         pooler = self.dropout(pooler)
         output = self.classifier(pooler)
@@ -118,22 +125,22 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
 
 # to solve data imbalance problem
-class_weights = compute_class_weight('balanced', classes=np.unique(df['score']), y=df['score'])
-class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
+# class_weights = compute_class_weight('balanced', classes=np.unique(df['score']), y=df['score'])
+# class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
 
-print(class_weights)
+# print(class_weights)
 
 # we are using sparse categorial crossentropy, because its for multi-class classification
 # loss_function = torch.nn.CrossEntropyLoss(weight=torch.tensor(class_weights, dtype=torch.float).to(device))
 loss_function = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.AdamW(params = model.parameters(), lr=LEARNING_RATE)
 
-def calcuate_mse(preds, targets):
-    mse_loss_fn = torch.nn.MSELoss()
-    preds = preds.float()
-    targets = targets.float()
-    mse_loss = mse_loss_fn(preds, targets)
-    return mse_loss.item()
+# def calcuate_mse(preds, targets):
+#     mse_loss_fn = torch.nn.MSELoss()
+#     preds = preds.float()
+#     targets = targets.float()
+#     mse_loss = mse_loss_fn(preds, targets)
+#     return mse_loss.item()
 
 # Training function
 def train(epoch):
@@ -141,7 +148,7 @@ def train(epoch):
     n_correct = 0
     nb_tr_steps = 0
     nb_tr_examples = 0
-    total_mse = 0
+    # total_mse = 0
     model.train()
     start_time = time.time()
     print(f"Training Epoch: {epoch}")
@@ -156,17 +163,17 @@ def train(epoch):
         loss = loss_function(outputs, targets)
         tr_loss += loss.item()
         big_val, big_idx = torch.max(outputs.data, dim=1)
-        mse = calcuate_mse(big_idx, targets)
-        total_mse += mse
+        # mse = calcuate_mse(big_idx, targets)
+        # total_mse += mse
 
         nb_tr_steps += 1
         nb_tr_examples += targets.size(0)
 
         if _ % 100 == 0:
             loss_step = tr_loss / nb_tr_steps
-            avg_mse = total_mse / nb_tr_steps
+            # avg_mse = total_mse / nb_tr_steps
             print(f"Training Loss per 100 steps: {loss_step}")
-            print(f"Training MSE per 100 steps: {avg_mse}")
+            # print(f"Training MSE per 100 steps: {avg_mse}")
             print(f"Time elapsed: {time.time() - start_time:.2f}s")
             start_time = time.time()
 
@@ -179,7 +186,7 @@ def train(epoch):
     epoch_loss = tr_loss/nb_tr_steps
     epoch_accu = (n_correct)/nb_tr_examples
     print(f"Training Loss Epoch: {epoch_loss}")
-    print(f"Training MSE Epoch: {epoch_accu}")
+    # print(f"Training MSE Epoch: {epoch_accu}")
 
     return
 
@@ -198,23 +205,23 @@ def valid(model, testing_loader):
             loss = loss_function(outputs, targets)
             tr_loss += loss.item()
             big_val, big_idx = torch.max(outputs.data, dim=1)
-            mse = calcuate_mse(big_idx, targets)
-            total_mse += mse
+            # mse = calcuate_mse(big_idx, targets)
+            # total_mse += mse
 
             nb_tr_steps += 1
             nb_tr_examples += targets.size(0)
 
             if _ % 100 == 0:
                 loss_step = tr_loss / nb_tr_steps
-                avg_mse = total_mse / nb_tr_steps
+                # avg_mse = total_mse / nb_tr_steps
                 print(f"Validation Loss per 100 steps: {loss_step}")
-                print(f"Validation MSE per 100 steps: {avg_mse}")
+                # print(f"Validation MSE per 100 steps: {avg_mse}")
                 print(f"Time elapsed: {time.time() - start_time:.2f}s")
                 start_time = time.time()
     epoch_loss = tr_loss/nb_tr_steps
     epoch_accu = (n_correct)/nb_tr_examples
     print(f"Validation Loss Epoch: {epoch_loss}")
-    print(f"Validation MSE Epoch: {epoch_accu}")
+    # print(f"Validation MSE Epoch: {epoch_accu}")
 
     return epoch_accu
 
@@ -223,14 +230,14 @@ if __name__ == "__main__":
     for epoch in range(EPOCHS):
         train(epoch)
 
-    output_model_file = './trained/v7.pth'
+    output_model_file = './trained/v8.pth'
 
     model_to_save = model
-    torch.save(model_to_save, output_model_file)
+    torch.save(model_to_save.state_dict(), output_model_file)
 
     # Uncomment when validating
     acc = valid(model, val_loader)
     print("Accuracy on test data = %0.2f%%" % acc)
 
     model_to_save = model
-    torch.save(model_to_save, output_model_file)
+    torch.save(model_to_save.state_dict(), output_model_file)
